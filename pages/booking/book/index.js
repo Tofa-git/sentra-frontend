37797332f1e2652e61d63b1871068a8ca4e135e2
Agung Wicaksono S <wicaksono1404@gmail.com>
2023-11-29 14:49:53 +0700

    Adding Booking Fixed

--- pages/booking/book/index.js
+++ pages/booking/book/index.js
@@ -271,12 +271,11 @@ const Index = (props) => {
 
     const _recheck = await recheckBookSearch(body);
     setRecheckData(_recheck.data.data);
-
+console.log(_recheck.data.data)
     if (_recheck) {      
-      setSessionId(_recheck.data.data.sessionId == null ?room.sessionId :  _recheck.data.data.sessionId)
-      console.log(sessionId)
+      setSessionId(_recheck.data.data.sessionId == null ?room.sessionId :  _recheck.data.data.sessionId)      
       setSupplierId(room.supplierId)
-      setHotelCode(_recheck.data.data.hotelCode)
+      setHotelCode(_recheck.data.data)
     }
 
     setTimeout(() => {
